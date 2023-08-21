957a60fdd8e5be1b52bee3dbf9d5247a90d769e7
gitForDavidL <davidtach26@gmail.com>
2023-08-21 11:27:55 -0500

    decript ok

--- desencriptar.py
+++ desencriptar.py
@@ -1,11 +1,15 @@
 mensaje = "THZEIPHMRRRGOSRKRUDWVLKNUSITAGSOKOEPHMRRRG"
 clave = "EDGAR"
 
-mensaje_descifrado = ""
-for i in range(len(mensaje)):
-    letra_mensaje = ord(mensaje[i]) - ord('A')
-    letra_clave = ord(clave[i % len(clave)]) - ord('A')
-    letra_descifrada = chr(((letra_mensaje + letra_clave) % 26) + ord('A'))
-    mensaje_descifrado += letra_descifrada
+##desencriptar el mensaje transfromando a en a 
+def desencriptar(mensaje, clave):
+    mensaje_desencriptado = ""
+    for i in range(len(mensaje)):
+        letra_mensaje = mensaje[i]
+        letra_clave = clave[i % len(clave)]
+        letra_desencriptada = chr((ord(letra_mensaje) - ord(letra_clave)) % 26 + ord('A'))
+        mensaje_desencriptado += letra_desencriptada
+    return mensaje_desencriptado
 
-print("El mensaje descifrado es:", mensaje_descifrado)
+mensaje_desencriptado = desencriptar(mensaje, clave)
+print("El mensaje desencriptado es:", mensaje_desencriptado)
